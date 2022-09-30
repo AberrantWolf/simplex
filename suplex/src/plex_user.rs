283a73c9c4f3a1ae4cae1e8a04729a5b91ed250a
Jer <alphastrata@gmail.com>
2022-09-30 22:57:57 +0800

    chore: cleanup, fix your broken attrs macro for a working one, add-in regex builder (for interest ,let's pick one and use it forever later)

--- suplex/src/plex_user.rs
+++ suplex/src/plex_user.rs
@@ -1,10 +1,14 @@
+//!
+//! Suplex `PlexUser`
+//!
 use crate::util::{
-    apply_plex_client_identifier, apply_plex_product, apply_plex_version, extract_xml_attr, Result,
+    apply_plex_client_identifier, apply_plex_product, apply_plex_version, Result, REGEX_SET,
 };
-use crate::SuplexError;
+use crate::{build_plex_user_add_fn, xml_match_attrs, SuplexError};
 
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
+use regex::Regex;
 use reqwest::header::HeaderMap;
 
 #[derive(Default, Debug, Clone)]
@@ -31,9 +35,17 @@ impl PlexUser {
         if let Ok(text) = result {
             println!("{}", text);
             if let Ok(result) = Self::_from_xml_text(text).await {
+                //if let Ok(Self::build_with_regex(&text)){ // Absolutely not faster :(
                 Some(result)
             } else {
                 // TODO: Log the error...?
+                // NOTE: I actually think custom result types would be nicer here, see the
+                // additions to SuplexError I added, you'd construct them like this:
+                //  Err(SuplexError::FetchFailure {
+                //      code: 400,
+                //      msg: "bad request".to_string(),
+                //  });
+                // which would let you have the codes you mentioned in a nice way maybe?
                 None
             }
         } else {
@@ -41,6 +53,12 @@ impl PlexUser {
             None
         }
     }
+
+    // This will build an add method, which add's &str to a PlexUser's appropriate field by
+    // matching substrings.
+    build_plex_user_add_fn!(
+        "email", email, "username", username, "title", title, "en", auth_token, "uuid", uuid
+    );
 
     async fn _try_login<T>(username: &T, password: &T) -> Result<String>
     where
@@ -73,37 +91,47 @@ impl PlexUser {
             match reader.read_event() {
                 Err(e) => return Err(SuplexError::XmlError { source: e }),
                 Ok(Event::Eof) => break,
-                Ok(Event::Start(e)) => match e.name().as_ref() {
-                    b"user" => {
-                        // id
-                        extract_xml_attr!(id, e, plex_user);
-
-                        // uuid
-                        if let Some(attr) = e.try_get_attribute("uuid")? {
-                            plex_user.uuid = attr.unescape_value()?.to_string();
-                        }
-
-                        // id
-                        if let Some(attr) = e.try_get_attribute("email")? {
-                            plex_user.email = attr.unescape_value()?.to_string();
-                        }
-
-                        // username
-                        if let Some(attr) = e.try_get_attribute("username")? {
-                            plex_user.username = attr.unescape_value()?.to_string();
-                        }
-
-                        // authToken
-                        // TODO: The current macro won't handle this because the two are different
-                        if let Some(attr) = e.try_get_attribute("authToken")? {
-                            plex_user.auth_token = attr.unescape_value()?.to_string();
-                        }
-                    }
-                    _ => (),
-                },
+                Ok(Event::Start(e)) => {
+                    xml_match_attrs!(plex_user, e, "id", "uuid", "username", "authToken")
+                }
                 _ => (),
             }
         }
         Ok(plex_user)
+    }
+    // INNER -- create Self from XML text String
+    /// Builds a [`PlexUser`] using Regexes and String-matching, rather than the
+    /// [`from_xml_text()`]'s line-by-line approach.
+    pub fn _build_with_regex(xml: &str) -> Result<Self> {
+        // //TODO: lazy static this -- it's bad if they're re-compiled in a loop.
+        // let set = RegexSet::new(&[
+        //     r#"(email="[a-z]+@[a-z]+.[a-z]+)"#,
+        //     r#"(id="\d+")"#,
+        //     r#"(uuid="\w+")"#,
+        //     r#"(username="\w+")"#,
+        //     r#"(title="\w+")"#,
+        //     r#"(authToken="\w+")"#, //NOTE: there's many.. variants of this?
+        // ])?;
+
+        let regexes: Vec<_> = REGEX_SET
+            .patterns()
+            // .into_par_iter() // t1.elapsed().as_nanos() = 2459129
+            .into_iter() // t1.elapsed().as_nanos() = 1602265
+            .map(|pat| Regex::new(pat).unwrap())
+            .collect();
+
+        let mut pu = PlexUser {
+            ..Default::default()
+        };
+
+        REGEX_SET
+            .matches(&xml)
+            .into_iter()
+            .map(|match_idx| &regexes[match_idx])
+            .flat_map(|pat| pat.find(&xml))
+            .for_each(|res| {
+                _ = pu.add(res.as_str());
+            });
+        Ok(pu)
     }
 }
